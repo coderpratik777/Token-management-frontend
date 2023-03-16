25b7ca8e9be123e1a24798ab3019969954867e3d
Aniket Tote <anikettote2001@gmail.com>
2023-03-16 16:45:59 +0530

    added admin and counter executive login

--- src/components/AdminLogin.jsx
+++ src/components/AdminLogin.jsx
@@ -1,11 +1,94 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const AdminLogin = () => {
+  const [userData, setUserData] = useState({ username: "", password: "" });
   return (
-    <div>
-      Admin login
-    </div>
-  )
-}
+    <section className="bg-gray-50">
+      <div className="flex flex-col items-center px-6 mx-auto py-12 lg:py-24">
+        <div className="w-full bg-white rounded-lg shadow-xl md:mt-0 sm:max-w-md xl:p-0 ">
+          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+            <h1 className="text-xl font-bold leading-tight w-full text-center tracking-tight text-gray-900 md:text-2xl ">
+              Admin Login
+            </h1>
+            <form
+              className="space-y-4 md:space-y-6"
+              action="#"
+              onSubmit={async (e) => {
+                e.preventDefault();
+                console.log(userData);
+              }}
+            >
+              <div>
+                <label
+                  htmlFor="email"
+                  className="block mb-2 text-sm font-medium text-gray-900 "
+                >
+                  Username
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
+                  placeholder="JhonDoe"
+                  required
+                  value={userData.username}
+                  onChange={(e) => {
+                    setUserData((prevData) => {
+                      return { ...prevData, username: e.target.value };
+                    });
+                  }}
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="password"
+                  className="block mb-2 text-sm font-medium text-gray-900 "
+                >
+                  Password
+                </label>
+                <input
+                  type="password"
+                  name="password"
+                  id="password"
+                  placeholder="••••••••"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
+                  required
+                  autoComplete="on"
+                  value={userData.password}
+                  onChange={(e) => {
+                    setUserData((prevData) => {
+                      return { ...prevData, password: e.target.value };
+                    });
+                  }}
+                />
+              </div>
+              <div className="flex items-center justify-between">
+                <div className="flex items-start">
+                  <div className="flex items-center h-5">
+                    <input
+                      id="remember"
+                      aria-describedby="remember"
+                      type="checkbox"
+                      className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 "
+                      required=""
+                    />
+                  </div>
+                </div>
+              </div>
+              <button
+                type="submit"
+                className="w-full text-white bg-slate-800 hover:bg-slate-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+              >
+                Sign in
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default AdminLogin
+export default AdminLogin;
--- src/components/AllCountersPanel.jsx
+++ src/components/AllCountersPanel.jsx
@@ -8,7 +8,7 @@ const AllCountersPanel = () => {
         <div className="container px-5 py-24 mx-auto">
           <div className="flex flex-wrap -m-4">
             <Link to="/counter" className="p-4 lg:w-1/4">
-              <div className="h-full bg-gray-100 bg-opacity-75 px-8 pt-16 pb-24 rounded-lg overflow-hidden text-center relative hover:shadow-lg hover:shadow-lg">
+              <div className="h-full bg-gray-100 bg-opacity-75 px-8 pt-16 pb-24 rounded-lg overflow-hidden text-center relative hover:shadow-lg">
                 <h1 className="title-font sm:text-2xl text-xl font-medium text-gray-900 mb-3">
                   Counter 1
                 </h1>
