8f12d0b640b08f84c5939ae1b76bf10c5ec05ec7
Aniket Tote <anikettote2001@gmail.com>
2023-03-16 16:48:00 +0530

    added admin and counter executive login

--- src/components/AdminLogin.jsx
+++ src/components/AdminLogin.jsx
@@ -64,19 +64,7 @@ const AdminLogin = () => {
                   }}
                 />
               </div>
-              <div className="flex items-center justify-between">
-                <div className="flex items-start">
-                  <div className="flex items-center h-5">
-                    <input
-                      id="remember"
-                      aria-describedby="remember"
-                      type="checkbox"
-                      className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 "
-                      required=""
-                    />
-                  </div>
-                </div>
-              </div>
+              
               <button
                 type="submit"
                 className="w-full text-white bg-slate-800 hover:bg-slate-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
